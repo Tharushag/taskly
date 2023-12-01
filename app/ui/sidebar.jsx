bad5a04923226272f2282fd9cfbc45e47a0f11a7
Tharusha <tharushadgunawardane@gmail.com>
2023-12-01 23:34:25 +0530

    implement add project functionality

--- app/ui/sidebar.jsx
+++ app/ui/sidebar.jsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
 import Tab from "@/app/ui/tab";
 import projects from "@/app/lib/placeholder";
 
@@ -14,7 +16,11 @@ function SideBar() {
     setTitles(
       titles.filter((t, i) => i !== id)
     );
+
+    setActiveTab(0);
   }
+
+  const addProject = () => setTitles([...titles, '']);
 
   const [activeTab, setActiveTab] = useState(0);
 
@@ -37,6 +43,14 @@ function SideBar() {
     <div className="bg-[#1E1E1E] rounded-3xl w-80 p-6 h-[calc(100vh-172px)] overflow-y-auto hide-scrollbar 
     flex flex-col gap-4 box-border">
       {createTabs()}
+
+      <IconButton 
+        aria-label="add" 
+        className="w-fit bg-[#2D2D2D] hover:bg-[#333333] rounded-2xl p-3"
+        onClick={addProject}
+      >
+        <AddIcon className="text-white opacity-70 text-lg" />
+      </IconButton>      
     </div>
   );
 }
